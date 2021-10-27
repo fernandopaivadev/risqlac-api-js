740b8e213707278a0ce7181ea2ad36e5a9076eb0
Fernando Paiva <fernandopaiva@techamazon.tech>
2021-10-27 11:54:54 -0300

    refactor(product controller): the methods 'create' and 'update' now require admin authentication

--- src/http/controllers/product.ts
+++ src/http/controllers/product.ts
@@ -5,17 +5,23 @@ import { CustomRequest } from '@types'
 
 export default {
   create: async (req: CustomRequest, res: Response): Promise<void> => {
-    const body = {
-      ...req.body
+    const { user } = req
+
+    if (user?.is_admin) {
+      const body = {
+        ...req.body
+      }
+
+      await prisma.product.create({
+        data: body
+      }).catch((err: any) => {
+        res.json(500).json({ message: err.message})
+      })
+
+      res.json(201).json({ message: 'product created' })
+    } else {
+      res.json(403).json({ message: 'admin access only' })
     }
-
-    await prisma.product.create({
-      data: body
-    }).catch((err: any) => {
-      res.json(500).json({ message: err.message})
-    })
-
-    res.json(201).json({ message: 'product created' })
   },
 
   list: async (req: CustomRequest, res: Response): Promise<void> => {
@@ -49,27 +55,33 @@ export default {
   },
 
   update: async (req: CustomRequest, res: Response): Promise<void> => {
-    const id = req.query?.id
+    const { user } = req
 
-    if (id) {
-      const body = {
-        ...req.body
-      }
+    if (user?.is_admin) {
+      const id = req.query?.id
 
-      const product = await prisma.product.update({
-        where: {
-          id: String(id)
-        },
-        data: body
-      })
+      if (id) {
+        const body = {
+          ...req.body
+        }
 
-      if (product) {
-        res.status(200).json({ product })
+        const product = await prisma.product.update({
+          where: {
+            id: String(id)
+          },
+          data: body
+        })
+
+        if (product) {
+          res.status(200).json({ product })
+        } else {
+          res.status(404).json({ message: 'product not found' })
+        }
       } else {
-        res.status(404).json({ message: 'product not found' })
+        res.status(412).json({ message: 'missing arguments' })
       }
     } else {
-      res.status(412).json({ message: 'missing arguments' })
+      res.status(403).json({ message: 'admin access only' })
     }
   },
 
