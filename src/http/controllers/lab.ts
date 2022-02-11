a71223b0fd73cc83802807e6b447b36bd46a8f58
Fernando <fernandopaivaec@gmail.com>
2022-02-10 22:38:27 -0300

    refactor(lab): update and delete routes now require specific permissions

--- src/http/controllers/lab.ts
+++ src/http/controllers/lab.ts
@@ -30,6 +30,12 @@ export default {
         next(err)
       })
 
+    if (labs) {
+      res.status(200).json({ labs })
+    } else {
+      res.status(404).json({ message: 'labs not found' })
+    }
+
     res.status(200).json({ labs })
   },
 
@@ -39,17 +45,9 @@ export default {
     const id = req.body?.id
 
     if (id) {
-      const lab = await prisma.lab.findFirst({
+      const lab = await prisma.lab.findUnique({
         where: {
-          id: String(id),
-          users: {
-            some: {
-              AND: {
-                user_id: user?.id,
-                access_level: 'admin'
-              }
-            }
-          }
+          id: String(id)
         }
       })
 
@@ -58,24 +56,32 @@ export default {
           ...req.body
         }
 
-        const updatedLab = await prisma.lab.update({
+        const userHasPermission = await prisma.users_on_labs.findFirst({
           where: {
-            id: lab?.id
-          },
-          data: body
-        }).catch((err: Error) => {
-          next(err)
+            AND: {
+              user_id: user?.id,
+              lab_id: String(lab.id),
+              access_level: 'admin'
+            }
+          }
         })
 
-        if (updatedLab) {
-          res.status(200).json({ lab: updatedLab })
+        if (userHasPermission) {
+          await prisma.lab.update({
+            where: {
+              id: lab?.id
+            },
+            data: body
+          }).catch((err: Error) => {
+            next(err)
+          })
+
+          res.status(200).json({ message: 'lab updated' })
         } else {
-          res.status(404).json({ message: 'lab not found' })
+          res.status(403).json({ message: 'admin access only' })
         }
       } else {
-        res.status(404).json({
-          message: 'this lab does not exist or you do not have permission to modify it'
-        })
+        res.status(404).json({ message: 'lab not found' })
       }
     } else {
       res.status(412).json({ message: 'missing arguments' })
@@ -85,26 +91,28 @@ export default {
   delete: async (req: CustomRequest, res: Response, next: NextFunction): Promise<void> => {
     const { user } = req
 
-    if (user?.is_admin) {
-      const id = req.query?.id
+    const id = req.query?.id
 
-      if (id) {
-        const lab = await prisma.lab.findFirst({
+    if (id) {
+      const lab = await prisma.lab.findUnique({
+        where: {
+          id: String(id)
+        }
+      })
+
+      if (lab) {
+        const userHasPermission = await prisma.users_on_labs.findFirst({
           where: {
-            id: String(id),
-            users: {
-              some: {
-                AND: {
-                  user_id: user?.id,
-                  access_level: 'admin'
-                }
-              }
+            AND: {
+              user_id: user?.id,
+              lab_id: String(lab.id),
+              access_level: 'admin'
             }
           }
         })
 
-        if (lab) {
-          const deletedLab = await prisma.lab.delete({
+        if (userHasPermission) {
+          await prisma.lab.delete({
             where: {
               id: lab.id
             }
@@ -112,21 +120,15 @@ export default {
             next(err)
           })
 
-          if (deletedLab) {
-            res.status(200).json({ lab: deletedLab })
-          } else {
-            res.status(404).json({ message: 'lab not found' })
-          }
+          res.status(200).json({ message: 'lab deleted' })
         } else {
-          res.status(404).json({
-            message: 'this lab does not exist or you do not have permission to modify it'
-          })
+          res.status(403).json({ message: 'admin access only' })
         }
       } else {
-        res.status(412).json({ message: 'missing arguments' })
+        res.status(404).json({ message: 'lab not found' })
       }
     } else {
-      res.status(403).json({ message: 'admin access only' })
+      res.status(412).json({ message: 'missing arguments' })
     }
   }
 }
